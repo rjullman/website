fb167e96cdc0db1ed419ace78a73d94569c00fb6
Robert Ullman <bullman@Roberts-MacBook-Pro.local>
2015-07-29 15:19:25 -0400

    changed email formating in intro blurb

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
       I am an experienced computer scientist with specific interest in 
       <emph>backend systems</emph>. I graduated from <emph>Princeton 
       University</emph> studying <emph>computer science and mathematics</emph>.  Feel 
-      free to contact me at <emph>robertjullman [at] gmail [dot] com</emph>.
+      free to contact me at <emph>robert.j.ullman [at] gmail [dot] com</emph>.
     </p>
 
     <iframe src='http://embed.verite.co/timeline/?source=0AiEbPoskFx2ddGdoY01kVjA1VnBXYjBQWWotclR5VkE&font=Bevan-PotanoSans&maptype=toner&lang=en&start_at_end=true&height=650' width='100%' height='650' frameborder='0'></iframe>
